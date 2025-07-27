3d7a68d78b0abdf82ed957c1a71ec682e68f422b
ipvHEEL <andrew0rohov2005@gmail.com>
2025-07-27 19:09:27 +0300

    Fix

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"ToDo-API/database"
+	"ToDo-API/server"
+	"time"
 )
 
 func main() {
 	database.Connect()
+	go func() {
+		server.RunServer()
+	}()
+	time.Sleep(100 * time.Millisecond)
+	database.UserInput()
+
+	select {}
 }
